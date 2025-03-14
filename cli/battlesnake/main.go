f23aa704661dc1331e98521cd967139f16290408
pikle <78937305+mrpikle@users.noreply.github.com>
2025-03-14 10:27:53 -0500

    test

--- cli/battlesnake/main.go
+++ cli/battlesnake/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/Pikle2/rules/cli/commands"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+
+	"github.com/Pikle2/rules/cli/commands"
+)
 
 func main() {
 	commands.Execute()
+	out, err := exec.Command(" battlesnake play -W 11 -H 11 --name soup --url http://0.0.0.0:8000 -g -solo --browser").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("test %s\n", out)
 }
